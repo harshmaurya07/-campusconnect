2f4f32e56c40848f1539677555d73bd0b605100e
GAMING MAURYA <hsmaurya134@gmail.com>
2026-01-09 17:37:44 +0000

    studio-3678853318-83496.firebasestorage.app     this is the storage buck

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,5 +3,6 @@ export const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: "https://studio-3678853318-83496-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://studio-3678853318-83496-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  storageBucket: "studio-3678853318-83496.firebasestorage.app"
 };
