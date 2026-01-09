16e37f7b77f4cac3b826f5f91117360aacfe244d
GAMING MAURYA <hsmaurya134@gmail.com>
2026-01-09 16:31:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3678853318-83496",
+  "appId": "1:941533185136:web:2bc8234be87311984fa0b5",
+  "apiKey": "AIzaSyCCc8SV6YibQtp2wc6rHRWIGSxhYxmvV3E",
+  "authDomain": "studio-3678853318-83496.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "941533185136"
+};
